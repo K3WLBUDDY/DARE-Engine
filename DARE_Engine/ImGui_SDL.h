97bf4f8ff9f1265cb04ae8d2d95d37eab3982653
Sruthik P <psruthik@gmail.com>
2016-12-29 14:01:56 +0530

    Messy addition of Imgui into engine

---
+++ DARE_Engine/ImGui_SDL.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include<SDL\SDL.h>
+#include<SDL\SDL_syswm.h>
+#include<imgui\imgui.h>
+#include<GL\glew.h>
+
+struct SDL_Window;
+typedef union SDL_Event SDL_Event;
+
+IMGUI_API bool        ImGui_ImplSdlGL3_Init(SDL_Window* window);
+IMGUI_API void        ImGui_ImplSdlGL3_Shutdown();
+IMGUI_API void        ImGui_ImplSdlGL3_NewFrame(SDL_Window* window);
+IMGUI_API bool        ImGui_ImplSdlGL3_ProcessEvent(SDL_Event* event);
+
+// Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_API void        ImGui_ImplSdlGL3_InvalidateDeviceObjects();
+IMGUI_API bool        ImGui_ImplSdlGL3_CreateDeviceObjects();
